eb1b5f4eb97e910304031cf0d1319147eb81f0fd
yourlocalmaccas <codingacc30@gmail.com>
2025-09-17 11:06:14 +1000

    Fixed the no display issue by doing "pygame.display.init" instead of "pygame.init"

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
 import pygame
-pygame.init()
+print("Before pygame.init()")
+pygame.display.init()
+print("After pygame.init()")
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("HSIE Game")
 
 running = True
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-	screen.fill((0, 0, 0))
-	pygame.display.flip()
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
 
 pygame.quit()
