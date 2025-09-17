a6c3cbcf48013a84404e6b2bb3b6211aa0b710ed
yourlocalmaccas <codingacc30@gmail.com>
2025-09-17 10:21:41 +1000

    foundation and black screen

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("HSIE Game")
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	screen.fill((0, 0, 0))
+	pygame.display.flip()
+
+pygame.quit()
